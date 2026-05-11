dff894fae6d3c82efc13fa51b3005019e58aa402
cguevara2626 <38379524+cguevara2626@users.noreply.github.com>
2026-05-11 17:15:17 +0000

    Primer commit: estructura del proyecto Android

---
+++ app/src/main/java/com/example/miapp/MainActivity.kt
@@ -0,0 +1,39 @@
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "_1Hola, name! Tu primera app en Kotlin + Codespaces",
+        fontSize = 24.sp,
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MaterialTheme { Greeting("Mundo") }
+}
